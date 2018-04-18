dd1d4f1b9e5f0f3dd19d09e2ec55c8fb3fc09d08
unknown <dkapper01@gmail.com>
2018-04-18 13:17:38 -0400

    display list of video

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 import SearchBar from "./search_bar";
 
 const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem video={video} />
+    });
     return (
-        <div className="col-md-4 list-group">
-            {props.videos.length}
-        </div>
+        <ul className="col-md-4 list-group">
+            {videoItems}
+        </ul>
     );
 };
 
