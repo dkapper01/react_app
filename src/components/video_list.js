62bf5e06c3481b6a2f74f32771b691338e2ca4b5
unknown <dkapper01@gmail.com>
2018-04-18 13:27:48 -0400

    added key video etag

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,7 +4,7 @@ import SearchBar from "./search_bar";
 
 const VideoList = (props) => {
     const videoItems = props.videos.map((video) => {
-        return <VideoListItem video={video} />
+        return <VideoListItem key={video.etag} video={video} />
     });
     return (
         <ul className="col-md-4 list-group">
